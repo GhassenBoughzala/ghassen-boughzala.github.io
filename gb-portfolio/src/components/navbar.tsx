e04269d14925cbc915ef17882189e92c5d64d759
Ghassen Boughzala <boughzala.ghassen@gmail.com>
2024-12-18 16:06:42 +0100

    clean Navbar & Footer

--- gb-portfolio/src/components/navbar.tsx
+++ gb-portfolio/src/components/navbar.tsx
@@ -10,21 +10,7 @@ function navbar() {
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Link</a>
-            </li>
-
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="bg-base-100 rounded-t-none p-2">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
+              <a>Contact</a>
             </li>
             <li>
               <Theme />
