ff7d77dce2dce9afed55518c6b0ac41b31704f31
Ghassen Boughzala <boughzala.ghassen@gmail.com>
2024-12-21 23:51:29 +0100

    .

--- gb-portfolio/src/components/navbar.tsx
+++ gb-portfolio/src/components/navbar.tsx
@@ -13,6 +13,9 @@ function navbar() {
               <a>Contact</a>
             </li>
             <li>
+              <a>Download my cv</a>
+            </li>
+            <li>
               <Theme />
             </li>
           </ul>
